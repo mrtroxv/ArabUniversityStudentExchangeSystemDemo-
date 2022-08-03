460869a966f91a19530700d56e000e75d247c9d9
assel al assel <aseel1234500000@gmail.com>
2022-08-03 18:47:03 +0300

    add arabic lang for the sidebar

--- src/layouts/VerticalLayout.js
+++ src/layouts/VerticalLayout.js
@@ -4,17 +4,22 @@ import { Outlet } from 'react-router-dom'
 // ** Core Layout Import
 // !Do not remove the Layout import
 import Layout from '@layouts/VerticalLayout'
+import { useTranslation } from 'react-i18next'
 
 // ** Menu Items Array
-import navigation from '@src/navigation/vertical'
+import MenuItems from '@src/navigation/vertical'
 
 const VerticalLayout = props => {
+
   // const [menuData, setMenuData] = useState([])
 
   // ** For ServerSide navigation
   // useEffect(() => {
   //   axios.get(URL).then(response => setMenuData(response.data))
   // }, [])
+  // send the custom hook to the function and use it
+  const { t } = useTranslation()
+  const navigation = MenuItems(t)
 
   return (
     <Layout menuData={navigation} {...props}>
