79ef06342a66c4727f1f01249e16ac0090fb5461
Huthaifa Salman <H.j.Salman@students.ptuk.edu.ps>
2022-10-27 09:06:35 +0300

    solved translation on refresh #23

--- src/pages/create-offer/OfferWizard.js
+++ src/pages/create-offer/OfferWizard.js
@@ -1,65 +1,75 @@
 // ** React Imports
-import { Fragment, useRef, useState } from 'react'
+import { Fragment, useRef, useState } from "react"
 
 // ** Custom Components
-import Wizard from '@components/wizard'
+import Wizard from "@components/wizard"
 
 // ** Steps
-import UniversityDetails from './components/UniversityDetails'
-import CompanyDetails from './components/CompanyDetails'
-import TrainingDetails from './components/TrainingDetails'
-import StudentDetails from './components/StudentDetails'
+import UniversityDetails from "./components/UniversityDetails"
+import CompanyDetails from "./components/CompanyDetails"
+import TrainingDetails from "./components/TrainingDetails"
+import StudentDetails from "./components/StudentDetails"
 
-import { Col, Card } from 'reactstrap'
-import { useTranslation } from 'react-i18next'
+import { Col, Card } from "reactstrap"
+import { useTranslation } from "react-i18next"
 
-const OfferWizard = () => {
-    // ** Ref
-    const ref = useRef(null)
+const OfferWizard = ({ outerSubmit, type }) => {
+  // ** Ref
+  const ref = useRef(null)
 
-    // ** State
-    const [stepper, setStepper] = useState(null)
+  // ** State
+  const [stepper, setStepper] = useState(null)
 
-    const [data, setData] = useState({})
-    const { t } = useTranslation()
-    const submitHandler = (data) => {
-        setData((prevData) => {
-            return { ...prevData, ...data }
-        })
-        console.log(data)
+  const [data, setData] = useState({})
+  const { t } = useTranslation()
+  const submitHandler = (data) => {
+    setData((prevData) => {
+      return { ...prevData, ...data }
+    })
+    outerSubmit()
+    console.log(data)
+  }
 
+  console.table(data)
+
+  const steps = [
+    {
+      id: "company-details",
+      title: t("instituteTab"),
+      subtitle: t("instituteTabSubTitle"),
+      content: <CompanyDetails stepper={stepper} onSubmit={submitHandler} />
+    },
+    {
+      id: "candidate-qualifications",
+      title: t("qualificationTab"),
+      subtitle: t("qualificationTabSubTitle"),
+      content: <StudentDetails stepper={stepper} onSubmit={submitHandler} />
+    },
+    {
+      id: "training-details",
+      title: t("trainingTab"),
+      subtitle: t("trainingTabSubTitle"),
+      content: (
+        <TrainingDetails
+          stepper={stepper}
+          onSubmit={submitHandler}
+          data={data}
+        />
+      )
     }
+  ]
 
-    console.table(data)
-
-    const steps = [
-        {
-            id: 'company-details',
-            title: t('instituteTab'),
-            subtitle: t('instituteTabSubTitle'),
-            content: <CompanyDetails stepper={stepper} onSubmit={submitHandler} />
-        },
-        {
-            id: 'candidate-qualifications',
-            title: t('qualificationTab'),
-            subtitle: t('qualificationTabSubTitle'),
-            content: <StudentDetails stepper={stepper} onSubmit={submitHandler} />
-        },
-        {
-            id: 'training-details',
-            title: t('trainingTab'),
-            subtitle: t('trainingTabSubTitle'),
-            content: <TrainingDetails stepper={stepper} onSubmit={submitHandler} data={data} />
-        }
-    ]
-
-    return (
-        <Col md="8" className='vertical-wizard'>
-            <Wizard type='vertical' options={{
-                linear: false
-            }} instance={el => setStepper(el)} ref={ref} steps={steps} />
-        </Col>
-    )
+  return (
+    <Wizard
+      type={type}
+      options={{
+        linear: false
+      }}
+      instance={(el) => setStepper(el)}
+      ref={ref}
+      steps={steps}
+    />
+  )
 }
 
 export default OfferWizard
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,156 +1,244 @@
 // eslint-disable-next-line
-import React, { Fragment, useState, useEffect } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import Breadcrumbs from '@components/breadcrumbs'
+import React, { Fragment, useState, useEffect } from "react"
+import { Routes, Route, Link } from "react-router-dom"
+import Breadcrumbs from "@components/breadcrumbs"
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardTitle,
-    CardText,
-    CardLink,
-    Row,
-    Col,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownItem,
-    DropdownMenu
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardLink,
+  Row,
+  Col,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  Badge,
+  Button,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Label,
+  Input,
+  ModalFooter
 } from "reactstrap"
 
-import UserDetails from './components/UserDetails'
+import UserDetails from "./components/UserDetails"
 // import TableBasic from './components/table/Table'
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from "react-i18next"
 
-import axios from 'axios'
-import OffersFilter from './components/OffersFilter'
-import ownedImage from '@src/assets/images/svg/icons8_box.svg'
-import sentImage from '@src/assets/images/svg/icons8_paper_plane.svg'
-import obtainedImage from '@src/assets/images/svg/icons8_post_office.svg'
-import activeImage from '@src/assets/images/svg/icons8_hard_working.svg'
-import DataTableWithButtons from './components/table/ReactTable'
-import { Archive, Edit, FileText, MoreVertical, Trash } from 'react-feather'
+import axios from "axios"
+import OffersFilter from "./components/OffersFilter"
+import ownedImage from "@src/assets/images/svg/icons8_box.svg"
+import sentImage from "@src/assets/images/svg/icons8_paper_plane.svg"
+import obtainedImage from "@src/assets/images/svg/icons8_post_office.svg"
+import activeImage from "@src/assets/images/svg/icons8_hard_working.svg"
+import DataTableWithButtons from "./components/table/ReactTable"
+import { Archive, Edit, FileText, MoreVertical, Trash } from "react-feather"
+import OfferWizard from "../create-offer/OfferWizard"
 
 function Home() {
-    // eslint-disable-next-line
-    const [offersList, setOffersList] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-    const [offersStatus, setOffersStatus] = useState("pending")
-    const cols = [
-        {
-          name: 'ID',
-          sortable: true,
-          maxWidth: '100px',
-          selector: row => row.id
-        },
-        {
-          name: 'Collage',
-          sortable: true,
-          minWidth: '225px',
-          selector: row => row.collage
-        },
-        {
-          name: 'Major',
-          sortable: true,
-          minWidth: '310px',
-          selector: row => row.major
-        },
-        {
-          name: 'Status',
-          sortable: true,
-          minWidth: '100px',
-          selector: row => row.status
-        },
-        {
-          name: 'Date',
-          sortable: true,
-          minWidth: '175px',
-          selector: row => row.end_date
-        },
-        {
-            name: 'Actions',
-            allowOverflow: true,
-            cell: () => {
-            return (
-                <div className='d-flex'>
-                <UncontrolledDropdown>
-                    <DropdownToggle className='pe-1' tag='span'>
-                    <MoreVertical size={15} />
-                    </DropdownToggle>
-                    <DropdownMenu end>
-                    <DropdownItem tag='a' href='/' className='w-100' onClick={e => e.preventDefault()}>
-                        <FileText size={15} />
-                        <span className='align-middle ms-50'>Details</span>
-                    </DropdownItem>
-                    <DropdownItem tag='a' href='/' className='w-100' onClick={e => e.preventDefault()}>
-                        <Archive size={15} />
-                        <span className='align-middle ms-50'>Archive</span>
-                    </DropdownItem>
-                    <DropdownItem tag='a' href='/' className='w-100' onClick={e => e.preventDefault()}>
-                        <Trash size={15} />
-                        <span className='align-middle ms-50'>Delete</span>
-                    </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <Edit size={15} />
-                </div>
-            )
-            }
-        }
-      ]
-    useEffect(() => {
-        axios.get('http://localhost:3500/offer/show_offer', {
-            headers: {
-                authorization: JSON.parse(localStorage.getItem('accessToken'))
-            }
-        })
-            .then(res => {
-                console.log(res.data)
-                setOffersList(res.data)
-                setFilteredData(res.data)
-            }).catch(err => {
-                console.log(err)
-            })
-    }, [])
-
-    useEffect(() => {
-        setFilteredData(offersList.filter(offer => {
-            return offer.status === offersStatus
-        }))
-    }, [offersStatus])
-    const { t } = useTranslation()
-    const viewTableHandler = (route) => {
-        setOffersStatus(route)
+  // eslint-disable-next-line
+  const [offersList, setOffersList] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+  const [offersStatus, setOffersStatus] = useState("pending")
+  const [formModal, setFormModal] = useState(false)
+  const { t } = useTranslation()
+  const cols = [
+    {
+      name: "ID",
+      sortable: true,
+      maxWidth: "25px",
+      selector: (row) => row.id
+    },
+    {
+      name: t("collage"),
+      sortable: true,
+      minWidth: "150px",
+      selector: (row) => row.collage
+    },
+    {
+      name: t("major"),
+      sortable: true,
+      minWidth: "300px",
+      selector: (row) => row.major
+    },
+    {
+      name: t("status"),
+      sortable: true,
+      minWidth: "50px",
+      selector: (row) => row.status
+    },
+    {
+      name: t("date"),
+      sortable: true,
+      minWidth: "175px",
+      selector: (row) => new Date(row.end_date).toLocaleDateString()
+    },
+    {
+      name: "Actions",
+      allowOverflow: true,
+      cell: (row) => {
+        return (
+          <div className="d-flex">
+            <UncontrolledDropdown>
+              <DropdownToggle className="pe-1" tag="span">
+                <MoreVertical size={15} />
+              </DropdownToggle>
+              <DropdownMenu end>
+                <DropdownItem
+                  tag="a"
+                  href="/"
+                  className="w-100"
+                  onClick={(e) => {
+                    e.preventDefault()
+                  }}
+                >
+                  <FileText size={15} />
+                  <span className="align-middle ms-50">Details</span>
+                </DropdownItem>
+                <DropdownItem
+                  tag="a"
+                  href="/"
+                  className="w-100"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <Archive size={15} />
+                  <span className="align-middle ms-50">Archive</span>
+                </DropdownItem>
+                <DropdownItem
+                  tag="a"
+                  href="/"
+                  className="w-100"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <Trash size={15} />
+                  <span className="align-middle ms-50">Delete</span>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <Edit
+              size={15}
+              onClick={(e) => {
+                e.preventDefault()
+                console.log(row.id)
+              }}
+            />
+          </div>
+        )
+      }
     }
+  ]
+  useEffect(() => {
+    axios
+      .get("http://localhost:3500/offer/show_offer", {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      .then((res) => {
+        console.log(res.data)
+        setOffersList(res.data)
+        setFilteredData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
-    return (
-        <Fragment>
-            <Breadcrumbs title={`${t('home')}`} data={[]} />
-            <Row className='match-height'>
-                <Col lg='6' md='12'>
-                    <Row className='match-height'>
-                        <Col lg='6' md='3' xs='6'>
-                            <OffersFilter onView={viewTableHandler} filter="owned" title="ownedOffers" src={ownedImage} />
-                        </Col>
-                        <Col lg='6' md='3' xs='6'>
-                            <OffersFilter onView={viewTableHandler} filter="sent" title="sentOffers" src={sentImage} />
-                        </Col>
-                        <Col lg='6' md='3' xs='6'>
-                            <OffersFilter onView={viewTableHandler} filter="obtained" title="obtainedOffers" src={obtainedImage} />
-                        </Col>
-                        <Col lg='6' md='3' xs='6'>
-                            <OffersFilter onView={viewTableHandler} filter="active" title="activeOffers" src={activeImage} />
-                        </Col>
-                    </Row>
-                </Col>
-                <Col lg='6' md='12'>
-                    <UserDetails />
-                </Col>
-            </Row>
-
-            <DataTableWithButtons data={filteredData} columns={cols} />
-
-        </Fragment>
+  useEffect(() => {
+    setFilteredData(
+      offersList.filter((offer) => {
+        return offer.status === offersStatus
+      })
     )
+  }, [offersStatus])
+  const viewTableHandler = (route) => {
+    setOffersStatus(route)
+  }
+  const handleOfferPopUp = () => {
+    setFormModal(!formModal)
+  }
+  return (
+    <Fragment>
+      <Breadcrumbs title={`${t("home")}`} data={[]} />
+      <Row className="match-height">
+        <Col lg="6" md="12">
+          <Row className="match-height">
+            <Col lg="6" md="3" xs="6">
+              <OffersFilter
+                onView={viewTableHandler}
+                filter="owned"
+                title="ownedOffers"
+                src={ownedImage}
+              />
+            </Col>
+            <Col lg="6" md="3" xs="6">
+              <OffersFilter
+                onView={viewTableHandler}
+                filter="sent"
+                title="sentOffers"
+                src={sentImage}
+              />
+            </Col>
+            <Col lg="6" md="3" xs="6">
+              <OffersFilter
+                onView={viewTableHandler}
+                filter="obtained"
+                title="obtainedOffers"
+                src={obtainedImage}
+              />
+            </Col>
+            <Col lg="6" md="3" xs="6">
+              <OffersFilter
+                onView={viewTableHandler}
+                filter="active"
+                title="activeOffers"
+                src={activeImage}
+              />
+            </Col>
+          </Row>
+        </Col>
+        <Col lg="6" md="12">
+          <UserDetails />
+        </Col>
+      </Row>
+
+      <Card>
+        <Row className="p-2">
+          <Col lg="12" md="12">
+            <Button color="primary" onClick={handleOfferPopUp}>
+              {t("createOffer")}
+            </Button>
+          </Col>
+        </Row>
+
+        <DataTableWithButtons data={filteredData} columns={cols} />
+      </Card>
+      {formModal && (
+        <Modal
+          isOpen={formModal}
+          toggle={() => setFormModal(!formModal)}
+          className="modal-dialog-centered modal-lg"
+        >
+          <ModalHeader
+            toggle={() => setFormModal(!formModal)}
+            className="modal-lg"
+          >
+            Add Offer
+          </ModalHeader>
+          <ModalBody>
+            <OfferWizard
+              outerSubmit={handleOfferPopUp}
+              type="modern-vertical"
+            />
+          </ModalBody>
+        </Modal>
+      )}
+    </Fragment>
+  )
 }
 
 export default Home
