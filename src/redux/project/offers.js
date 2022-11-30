72f9241dc8d85d9673a5965b37e514f4639f1fd4
HuzSalman <hsalman@restaurant365.com>
2022-11-30 08:12:17 +0200

    Modified offer actions and offer table actions, fixed selectors

--- src/redux/project/offers.js
+++ src/redux/project/offers.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
-// eslint-disable-next-line 
+// eslint-disable-next-line
 const OFFERS_URL = "http://localhost:3500/offer/show_offer"
 const CREATE_OFFER_URL = "http://localhost:3500/offer/insert_offer"
 const OWN_OFFERS_URL = "http://localhost:3500/offer/own-offers"
@@ -33,21 +33,22 @@ const initialState = {
   }
 }
 
-export const createOffer = createAsyncThunk("offers/createOffer", async (offerData) => {
-  let response
-  try {
-    response = await axios.post(CREATE_OFFER_URL, offerData,
-      {
+export const createOffer = createAsyncThunk(
+  "offers/createOffer",
+  async (offerData) => {
+    let response
+    try {
+      response = await axios.post(CREATE_OFFER_URL, offerData, {
         headers: {
           authorization: JSON.parse(localStorage.getItem("accessToken"))
         }
       })
-    return response.data
-  } catch (err) {
-    console.log(err)
+      return response.data
+    } catch (err) {
+      console.log(err)
+    }
   }
-
-})
+)
 
 // getAllOffers
 export const fetchAllOffers = createAsyncThunk("offers/getOffers", async () => {
@@ -63,99 +64,125 @@ export const fetchAllOffers = createAsyncThunk("offers/getOffers", async () => {
   }
 })
 
-export const fetchOwnOffer = createAsyncThunk("offers/getOwnOffers", async () => {
-  try {
-    const response = await axios.get(OWN_OFFERS_URL, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
+export const fetchOwnOffer = createAsyncThunk(
+  "offers/getOwnOffers",
+  async () => {
+    try {
+      const response = await axios.get(OWN_OFFERS_URL, {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
   }
-})
-
-export const fetchObtainedOffer = createAsyncThunk("offers/getObtainedOffers", async () => {
-  try {
-    const response = await axios.get(OBTAINED_OFFER_URL, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-export const fetchActiveOffers = createAsyncThunk("offers/getActiveOffers", async () => {
-  try {
-    const response = await axios.get(ACTIVE_OFFERS_URL, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-export const fetchEndedOffers = createAsyncThunk("offers/getEndedOffers", async () => {
-  try {
-    const response = await axios.get(ENDED_OFFERS_URL, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-export const sendOffer = createAsyncThunk("offers/sendOffer", async (offerData) => {
-  try {
-    const response = await axios.post(SEND_OFFER_URL, offerData, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-})
-export const deleteOffer = createAsyncThunk("offers/deleteOffer", async (offer_id) => {
-  try {
-    const response = await axios.post(DELETE_OFFER_URL, { offer_id }, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    console.log(response.data)
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-}
-)
-export const rejectOffer = createAsyncThunk("offers/rejectOffer", async (offer_id) => {
-  try {
-    const response = await axios.post(REJECT_OFFER_URL, { offer_id }, {
-      headers: {
-        authorization: JSON.parse(localStorage.getItem("accessToken"))
-      }
-    })
-    console.log(response.data)
-    return response.data
-  } catch (error) {
-    console.log(error)
-  }
-}
 )
 
+export const fetchObtainedOffer = createAsyncThunk(
+  "offers/getObtainedOffers",
+  async () => {
+    try {
+      const response = await axios.get(OBTAINED_OFFER_URL, {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
+export const fetchActiveOffers = createAsyncThunk(
+  "offers/getActiveOffers",
+  async () => {
+    try {
+      const response = await axios.get(ACTIVE_OFFERS_URL, {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
+export const fetchEndedOffers = createAsyncThunk(
+  "offers/getEndedOffers",
+  async () => {
+    try {
+      const response = await axios.get(ENDED_OFFERS_URL, {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
+export const sendOffer = createAsyncThunk(
+  "offers/sendOffer",
+  async (offerData) => {
+    try {
+      const response = await axios.post(SEND_OFFER_URL, offerData, {
+        headers: {
+          authorization: JSON.parse(localStorage.getItem("accessToken"))
+        }
+      })
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+export const deleteOffer = createAsyncThunk(
+  "offers/deleteOffer",
+  async (offer_id) => {
+    try {
+      const response = await axios.post(
+        DELETE_OFFER_URL,
+        { offer_id },
+        {
+          headers: {
+            authorization: JSON.parse(localStorage.getItem("accessToken"))
+          }
+        }
+      )
+      console.log(response.data)
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+export const rejectOffer = createAsyncThunk(
+  "offers/rejectOffer",
+  async (offer_id) => {
+    try {
+      const response = await axios.post(
+        REJECT_OFFER_URL,
+        { offer_id },
+        {
+          headers: {
+            authorization: JSON.parse(localStorage.getItem("accessToken"))
+          }
+        }
+      )
+      console.log(response.data)
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
 
 const offersSlice = createSlice({
   name: "offers",
@@ -166,7 +193,7 @@ const offersSlice = createSlice({
       state.deleteOfferState.error = null
     },
     deleteOfferFromStore: (state, action) => {
-      state.offers = state.offers.filter(offer => offer.id !== action.payload)
+      state.offers = state.offers.filter((offer) => offer.id !== action.payload)
     }
   },
   extraReducers: (builder) => {
@@ -219,7 +246,6 @@ const offersSlice = createSlice({
           state.createOfferState.status = "succeeded"
           state.createOfferState.error = null
         }
-
       })
       // eslint-disable-next-line
       .addCase(createOffer.rejected, (state, action) => {
@@ -234,13 +260,11 @@ const offersSlice = createSlice({
           state.sendOfferState.status = "succeeded"
           state.sendOfferState.error = null
         }
-
       })
       .addCase(sendOffer.rejected, (state, action) => {
         state.sendOfferState.status = null
         state.sendOfferState.error = action.payload.data.message
-      }
-      )
+      })
       .addCase(deleteOffer.fulfilled, (state, action) => {
         if (action.payload.status === 404) {
           state.deleteOfferState.status = null
@@ -249,14 +273,11 @@ const offersSlice = createSlice({
           state.deleteOfferState.status = "succeeded"
           state.deleteOfferState.error = null
         }
-
-      }
-      )
+      })
       .addCase(deleteOffer.rejected, (state, action) => {
         state.deleteOfferState.status = null
         state.deleteOfferState.error = action.payload.data.message
-      }
-      )
+      })
       .addCase(rejectOffer.fulfilled, (state, action) => {
         if (action.payload.status === 404) {
           state.rejectOfferState.status = null
@@ -265,14 +286,11 @@ const offersSlice = createSlice({
           state.rejectOfferState.status = "succeeded"
           state.rejectOfferState.error = null
         }
-
-      }
-      )
+      })
       .addCase(rejectOffer.rejected, (state, action) => {
         state.rejectOfferState.status = null
         state.rejectOfferState.error = action.payload.data.message
-      }
-      )
+      })
   }
 })
 
@@ -286,15 +304,13 @@ export const selectSentOffers = (state, userId) =>
   state.offers.offers.filter(
     (offer) =>
       offer.university_id_src === userId &&
-      offer.status === 1 &&
-      offer.status === 2
+      (offer.status === 1 || offer.status === 2)
   )
 export const selectObtainedOffers = (state, userId) =>
   state.offers.offers.filter(
     (offer) =>
       offer.university_id_des === userId &&
-      offer.status === 1 &&
-      offer.status === 2
+      (offer.status === 1 || offer.status === 2)
   )
 
 export const selectOfferById = (state, id) => {
@@ -309,5 +325,6 @@ export const selectCreateOfferState = (state) => state.offers.createOfferState
 export const selectDeleteOfferState = (state) => state.offers.deleteOfferState
 export const selectRejectOfferState = (state) => state.offers.rejectOfferState
 
-export const { resetDeleteOfferState, deleteOfferFromStore } = offersSlice.actions
+export const { resetDeleteOfferState, deleteOfferFromStore } =
+  offersSlice.actions
 export default offersSlice.reducer
